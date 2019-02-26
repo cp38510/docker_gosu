80f81ed707fbf361166fdd0ee6f25c3971f82359
Andrey Reshetnikov <a.s.reshetnikov@s7.ru>
2019-02-26 17:43:29 +0300

    change

--- init.sh
+++ init.sh
@@ -7,7 +7,8 @@ else
 mv ./Dockerfile ./bak_Dockerfile &> /dev/null
 mv ./entrypoint.sh ./bak_entrypoint.sh &> /dev/null
 #
-echo -e "FROM ubuntu:latest\nCOPY --from=gosu/assets /opt/gosu /opt/gosu\nCOPY entrypoint.sh /usr/local/bin/entrypoint.sh\n\nRUN set -x \\ \n    && /opt/gosu/gosu.install.sh \\ \n    && rm -fr /opt/gosu \\ \n    && apt-get update \\ \n    && apt-get install -y sudo curl ca-certificates \\ \n    && chmod +x /usr/local/bin/entrypoint.sh\n\nENTRYPOINT [\"/usr/local/bin/entrypoint.sh\"]" > ./Dockerfile
+#echo -e "FROM ubuntu:latest\nCOPY --from=gosu/assets /opt/gosu /opt/gosu\nCOPY entrypoint.sh /usr/local/bin/entrypoint.sh\n\nRUN set -x \\ \n    && /opt/gosu/gosu.install.sh \\ \n    && rm -fr /opt/gosu \\ \n    && apt-get update \\ \n    && apt-get install -y sudo curl ca-certificates \\ \n    && chmod +x /usr/local/bin/entrypoint.sh\n\nENTRYPOINT [\"/usr/local/bin/entrypoint.sh\"]" > ./Dockerfile
+echo -e "FROM ubuntu:latest\nCOPY entrypoint.sh /usr/local/bin/entrypoint.sh\n\nRUN set -x \\ \n        && apt-get update \\ \n        && apt-get install -y gosu sudo \\ \n        && rm -rf /var/lib/apt/lists/* \\ \n        && gosu nobody true \\ \n        && chmod +x /usr/local/bin/entrypoint.sh\n\nENTRYPOINT [\"/usr/local/bin/entrypoint.sh\"]" > ./Dockerfile
 if [[ -n "$1" ]]; then sed -i "s/ubuntu:latest/$1/g" ./Dockerfile; fi
 #
 echo -e '#!/bin/bash\nset -e\nCURRENT_UID=${uid:-9999}\nuseradd --shell /bin/bash -u $CURRENT_UID -o -c "" -m docker\nexport HOME=/home/docker' > ./entrypoint.sh
